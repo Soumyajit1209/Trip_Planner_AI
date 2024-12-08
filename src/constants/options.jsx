f969d15c4d61968b1c1f599e9ede4187d4445a67
Soumyajit Khan <soumyajitkhan6@gmail.com>
2024-12-08 19:29:42 +0530

    includes feature for CreateTrip

---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const selectTravelarList = [
+    {
+        id: 1,
+        title: "Me",
+        desc: "Traveling solo, just for yourself.",
+        icon: "🙋‍♂️", 
+        people:"1 People"
+      },
+      {
+        id: 2,
+        title: "Couple",
+        desc: "Perfect for romantic getaways.",
+        icon: "👩‍❤‍💋‍👨", // Example: Emoji for couple
+        people:"2 People"
+      },
+      {
+        id: 3,
+        title: "Family",
+        desc: "Ideal for family vacations with everyone.",
+        icon: "👨‍👩‍👧‍👦", // Example: Emoji for family
+        people: "3 to 5 People"
+      },
+      {
+        id: 4,
+        title: "Friend",
+        desc: "Great for trips with your best buddies.",
+        icon: "👯‍♂️", // Example: Emoji for friends
+        people: "5 to 10 People"
+      },
+  ];
+
+  export const selectBudgetOptions = [
+    {
+      id: 1,
+      title: "Budget-Friendly",
+      desc: "Affordable options for travelers on a budget.",
+      icon: "💸", // Example: Emoji for budget
+    },
+    {
+      id: 2,
+      title: "Mid-Range",
+      desc: "Balanced options offering comfort and affordability.",
+      icon: "💵", // Example: Emoji for mid-range
+    },
+    {
+      id: 3,
+      title: "Luxury",
+      desc: "Premium experiences for travelers seeking luxury.",
+      icon: "💎", // Example: Emoji for luxury
+    },
+  ];
+
+  export const AI_PROMPT = 'Generate Travel Plan for Location : {location}'
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,13 +1,94 @@
+import { selectBudgetOptions } from '@/constants/options';
+import { selectTravelarList } from '@/constants/options';
+import { Button } from '@/components/ui/button';
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 
 const CreateTrip = () => {
+  const [place, setplace] = useState();
+  const [formData, setformData] = useState([]);
+
+  const handleInputChange =(name, value)=>{
+      setformData({
+        ...formData,
+        [name] : value
+      })
+  }
+
+
+
+ useEffect(()=>{
+  console.log(formData);
+ },[formData])
+
+ const OnGenerateTrip = ()=>{
+  if(formData?.noOfDays > 5){
+    return;
+  }
+ }
+  
   return (
-    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5  mt-10'>
-     <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
-     <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip palnner will generate a customized itinerary based on your preferences </p>
-    </div>
+    <><div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5  mt-10'>
+      <h2 className='font-bold text-3xl'>Tell us your travel preferences 🏕️🌳</h2>
+      <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip palnner will generate a customized itinerary based on your preferences </p>
+      <div className='mt-20 flex flex-col gap-10'>
+        <div className='mt-20'>
+          <h2 className='text-xl my-3 font-medium'>What is destination of choice?</h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+            selectProps={{
+              place,
+              onChange: (v) => { setplace(v); handleInputChange('location' , v)}
+            }} />
+        </div>
+
+        <div>
+          <h2 className='text-xl my-3 font-medium'>How many days are you planning yout trip?</h2>
+          <input className='h-7 border-gray-400 border-2' placeholder={'Ex-3'} type="number"
+          onChange={(e)=> handleInputChange('noOfDays' , e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div>
+        <h2 className='text-xl my-7 font-medium'>What is Your Budget?</h2>
+        <div className='grid grid-cols-3 gap-5 mt-5'>
+          {selectBudgetOptions.map((items, index) => (
+            <div key={index}
+            onClick={()=>handleInputChange('budget' , items.title)}
+            className={`p-4 border rounded-lg cursor-pointer hover:shadow-lg
+             ${formData?.budget == items.title && 'shadow-md border-black'}
+            `}>
+              <h2 className='text-4xl'>{items.icon}</h2>
+              <h2 className='font-bold text-lg'>{items.title}</h2>
+              <h2 className='text-sm text-gray-500'>{items.desc}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <h2 className='text-xl my-7 font-medium'>Who do you plan on travelling with on your next adventure?</h2>
+        <div className='grid grid-cols-3 gap-5 mt-5'>
+          {selectTravelarList.map((items, index) => (
+            <div key={index}
+            onClick={()=>handleInputChange('traveler' , items.people)}
+            className={`p-4 border rounded-lg cursor-pointer hover:shadow-lg
+            ${formData?.traveler == items.people && 'shadow-lg border-black'}
+            `}>
+              <h2 className='text-4xl'>{items.icon}</h2>
+              <h2 className='font-bold text-lg'>{items.title}</h2>
+              <h2 className='text-sm text-gray-500'>{items.desc}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </div><Button onClick={OnGenerateTrip}>Generate Trip</Button>
+    </>
+    
   )
 }
 
